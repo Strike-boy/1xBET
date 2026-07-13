af53bf6c6d3c6e64eff42630f91b6425bc1e1313
Strike-boy <ogabekmahmudzonov24@gmail.com>
2026-07-13 17:07:05 +0500

    Remove subscription from package exports

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,5 @@
 """Пакет с обработчиками бота"""
 from . import client
 from . import admin
-from . import subscription
 
-__all__ = ['client', 'admin', 'subscription']
+__all__ = ['client', 'admin']
